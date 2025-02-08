4003352c4b7cecafe06ab8e3a038db22abf7d40e
Xottab-DUTY <xottab-duty@yandex.ru>
2025-02-08 13:23:31 +0500

    Revert "Ignore SDL window events if can't acquire the window"
    This reverts commit ae15e9e3b4999e86326b8d6154d77ebbc118417e.

--- src/xrEngine/x_ray.cpp
+++ src/xrEngine/x_ray.cpp
@@ -400,8 +400,6 @@ int CApplication::Run()
             case SDL_WINDOWEVENT:
             {
                 const auto window = SDL_GetWindowFromID(event.window.windowID);
-                if (!window)
-                    continue;
 
                 switch (event.window.event)
                 {
